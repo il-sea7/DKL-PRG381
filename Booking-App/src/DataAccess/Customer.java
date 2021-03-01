65a103c9d381ddd375274481f224878e5019a5ad
il-sea7 <itu.kev.lesu@gmail.com>
2021-03-01 23:02:54 +0200

    created DataAccessLayer

---
+++ Booking-App/src/DataAccess/Customer.java
@@ -0,0 +1,14 @@
+package DataAccess;
+
+public class Customer extends Person {
+
+    public Customer(String name, String surname, String phone) {
+        super(name, surname, phone);
+        // TODO Auto-generated constructor stub
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
---
+++ Booking-App/src/DataAccess/Person.java
@@ -0,0 +1,13 @@
+package DataAccess;
+
+public abstract class Person {
+
+    String name, surname, phone;
+
+    Person(String name, String surname, String phone) {
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+    }
+
+}
