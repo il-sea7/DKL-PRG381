9eba10a6a44ef9a4e974c0e8b7e326f062486848
il-sea7 <itu.kev.lesu@gmail.com>
2021-03-01 23:34:51 +0200

    Added get/set in Employee cs

--- Booking-App/src/DataAccess/Employee.java
+++ Booking-App/src/DataAccess/Employee.java
@@ -6,5 +6,23 @@ public class Employee extends Person {
         super(name, surname, phone);
         // TODO Auto-generated constructor stub
     }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+		this.name = name;
+	}
+    public String getSurname() {
+        return surname;
+    }
+    public void setSurname(String name) {
+		this.name = name;
+	}
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String name) {
+		this.name = name;
+	}
 
 }
