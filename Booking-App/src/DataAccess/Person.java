63d8e3d594b3cdea7149d785080409e9912a5ca7
Difference-Mabasa <veejay.bavhu@gmail.com>
2021-03-05 22:35:28 +0200

    Added login method in person class

--- Booking-App/src/DataAccess/Person.java
+++ Booking-App/src/DataAccess/Person.java
@@ -1,5 +1,7 @@
 package DataAccess;
 
+import java.util.Scanner;
+
 public abstract class Person {
 
     String name, surname, phone;
@@ -9,5 +11,29 @@ public abstract class Person {
         this.surname = surname;
         this.phone = phone;
     }
+
+    public void login() {
+
+        String custLogin = "cust";
+        String empLogin = "emp";
+        Boolean loggedIn = false;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter username");
+        String userInput = input.nextLine();
+
+        if (userInput == custLogin) {
+            loggedIn = true;
+        }
+
+        else if (userInput == empLogin) {
+
+        }
+
+        else {
+
+        }
+
+    };
 
 }
